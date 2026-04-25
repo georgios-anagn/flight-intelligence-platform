c94a135340cea2786f18288db9168a17b15e7f2f
Georgios Anagnostou <ganagnost@hotmail.com>
2026-04-25 21:20:46 +0200

    Airflow v1.0

---
+++ airflow/dags/dbt_daily_run.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator 
+from datetime import datetime, timedelta
+
+with DAG( 
+    dag_id='dbt_daily_run',
+    default_args={'owner':'airflow','retries':1, 
+                  'retry_delay':timedelta(minutes=5)},
+    schedule_interval='0 6 * * *', 
+    start_date=datetime(2024,1,1), 
+    catchup=False
+) as dag: 
+    BashOperator( 
+        task_id='run_dbt', 
+        bash_command='cd /opt/airflow/dbt && dbt run' 
+    )
+
+docker compose up -d
